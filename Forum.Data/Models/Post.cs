1918cbbb4ff84b121ed0c3a4ebfcc5a840be4c66
PiotrWodecki <piotr.wodecki.2@gmail.com>
2020-02-16 20:51:26 +0100

    Initial commit. Create Data Project.

---
+++ Forum.Data/Models/Post.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Forum.Data.Models
+{
+    public class Post
+    {
+        public int ID { get; set; }
+        public string Title { get; set; }
+        public string Content { get; set; }
+        public DateTime Created { get; set; }
+        public virtual ApplicationUser User { get; set; }
+        public virtual Forum Forum { get; set; }
+        public virtual IEnumerable<PostReply> PostReplies { get; set; }
+    }
+}
--- Forum/Startup.cs
+++ Forum/Startup.cs
@@ -1,15 +1,11 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using Forum.Data;
+using Forum.Data.Models;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using Forum.Data;
-using Forum.Models;
 using Forum.Services;
 
 namespace Forum
