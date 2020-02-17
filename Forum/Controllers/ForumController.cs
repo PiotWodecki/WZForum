fe30cecb1ada7b1a49315e99165f8c9f35725cd1
PiotrWodecki <piotr.wodecki.2@gmail.com>
2020-02-17 23:25:06 +0100

    Adding classes and razor view

---
+++ Forum/Controllers/ForumController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Forum.Data;
+using ForumWZ.Models.Forum;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ForumWZ.Controllers
+{
+    public class ForumController : Controller
+    {
+        private readonly IForum _forumService;
+        public ForumController(IForum forumService)
+        {
+            _forumService = forumService; //potrzebne do dependency injection
+            //.NEt za każdym razem jak prosimy o coś co implementuje IForum interfejs to daj konkretną instację
+            //ForumWZ.Service ale kontrolerowi potrzebne jest tylko jego zachowanie czyli to co robi
+        }
+
+        public IActionResult Index()
+        {
+            IEnumerable<ForumListingModel> forums = _forumService.GetAll().Select(forum=>new ForumListingModel
+            {
+                Id = forum.Id,
+                Name = forum.Title,
+                Description = forum.Description
+            });
+            var model = new ForumIndexModel
+            {
+                ForumList = forums
+            };
+            return View(model);
+        }
+    }
+}
---
+++ Forum/Models/Forum/ForumIndexModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ForumWZ.Models.Forum
+{
+    public class ForumIndexModel
+    {
+        public IEnumerable<ForumListingModel> ForumList { get; set; } //to nam posłuży do zapakowania ienumerable forumlistingmodel
+        //żeby przesłać to dalej kontrolerem
+    }
+}
