cee6a504b9764f66424eee9f5eb290ca077aef0d
PiotrWodecki <piotr.wodecki.2@gmail.com>
2020-03-22 12:36:27 +0100

    Register user, upload services - adding dependency injection

--- Forum.Service/UploadService.cs
+++ Forum.Service/UploadService.cs
@@ -1,10 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿using ForumWZ.Data;
 
 namespace ForumWZ.Service
 {
-    public class UploadService
+    public class UploadService : IUpload
     {
     }
 }
